3d5eae1a5dcec68389f18528838fe4be19a53523
Omer Gery <omergery1996@gmail.com>
2022-02-23 01:38:08 +0200

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,26 @@
+
+const application = require('express')();
+const server = require('http').createServer(application)
+const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+   console.log('Server is running on port: ' + PORT);
+});
+ 
+io.on('connection', (socket) => {
+ 
+   socket.on('disconnect', () => {
+       console.log('User disconnected - Username: ' + socket.username);
+   });
+ 
+   socket.on('message to server', (msg) => {
+       console.log(msg);
+       io.local.emit('message from server', msg);
+   });
+ 
+   socket.on('new user', (userName) => {
+       console.log(`User connected ${userName}`);
+       io.emit('message from server', `new user : hello to ${userName}!`);
+   });
+});
