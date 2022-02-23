fd5b901d1697981b2b952845519b21b7bb1550a4
Omer Gery <omergery1996@gmail.com>
2022-02-23 19:38:48 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
 
 const app = require('express')();
-const server = require('http').createServer(application)
+const server = require('http').createServer(app)
 const io = require('socket.io')(server);
-const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send(`__dirname/chat-client/src/App.js`)
-  })
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
    console.log('Server is running on port: ' + PORT);
 });
