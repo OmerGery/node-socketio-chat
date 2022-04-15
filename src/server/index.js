63dea7d3433d77d425e21aa96825066dfa73ea13
Omer Gery <omergery1996@gmail.com>
2022-04-16 00:35:38 +0300

    wip

--- src/server/index.js
+++ src/server/index.js
@@ -8,10 +8,7 @@ const getTimeNow = () => `[${moment().tz('Asia/Jerusalem').format('HH:mm:ss')}]`
 server.listen(PORT, () => {
    console.log(`Server is running on port: ${PORT}`);
 });
-app.get('/', (req,res) => {
-    res.sendFile('page.html', {root: './src/client'});
-})
- 
+
 io.on('connection', (socket) => {
  
    socket.on('disconnect', () => {
